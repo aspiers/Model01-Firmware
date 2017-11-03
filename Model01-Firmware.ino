64ced587e1219a8496e97ea8d98056f365b0a264
Adam Spiers <Model01-Firmware@adamspiers.org>
2017-11-04 10:35:07 +1100

    change PgDn -> #/~
    This matches the position of ~ on my Kinesis and also provides #
    since the Model 01 has £ on shift-3 with my current xkb setup.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -136,7 +136,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   (Key_Equals,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Backtick,
    Key_Minus,    Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_RightAlt,
+   Key_Backslash, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_RightAlt,
    Key_Backspace, Key_LeftControl, Key_LeftAlt, Key_Escape,
    Key_LeftShift,
 
